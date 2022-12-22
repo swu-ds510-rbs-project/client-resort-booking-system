6481e818bae395eb7f005457058376a842c5245d
sueshi-lab <phanpaporn.zcx@gmail.com>
2022-12-22 10:16:38 +0700

    Update Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Navbar, Nav } from 'rsuite';
-import HomeIcon from '@rsuite/icons/legacy/Home';
 import axios from 'axios';
 import { config } from '../config.js';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -51,18 +50,6 @@ export const TheNavbar = () => {
   return (
     <Navbar className="fs-6">
       <Navbar.Brand href="/" >RBS</Navbar.Brand>
-      <Nav>
-        <Nav.Item href="/" eventKey="1" icon={<HomeIcon />}>
-          Home
-        </Nav.Item>
-        <Nav.Item href="/" eventKey="2">News</Nav.Item>
-        <Nav.Item href="/" eventKey="3">Products</Nav.Item>
-        <Nav.Menu href="/" title="About">
-          <Nav.Item href="/" eventKey="4">Company</Nav.Item>
-          <Nav.Item href="/" eventKey="5">Team</Nav.Item>
-          <Nav.Item href="/" eventKey="6">Contact</Nav.Item>
-        </Nav.Menu>
-      </Nav>
 
       {!isLogin ?
         <Nav pullRight>
